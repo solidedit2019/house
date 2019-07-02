140244d71e27e0b6314c006acd6fe13431e3b225
wjb2 <superich.wjb@163.com>
2019-07-02 13:59:44 +0800

    老王新建的文件

---
+++ src/main/java/com/kgc/house/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.kgc.house.controller;
+
+/**
+ * @author 王建兵
+ * @Classname TestController
+ * @Description TODO
+ * @Date 2019/7/2 13:48
+ * @Created by Administrator
+ */
+
+public class TestController {
+
+}
